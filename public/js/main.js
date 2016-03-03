4acd332f183d983001e42ff1489ea1e0d44b7a7f
Red-French <red@redfrench.net>
2016-03-03 15:31:14 -0600

    add public/vendor

---
+++ public/js/main.js
@@ -0,0 +1,48 @@
+'use strict';
+;(function () {  // start iffy with a semicolon to prevent potential error because of what came before not having a semicolon
+
+    const ws = io.connect();
+
+    ws.on('connect', () => {  // event Listener
+      console.log('******* FRONT END socket connected *******')
+    });
+
+    ws.on('receiveChat', msg => {  // event Listener
+      console.log(msg);
+      displayChat(msg.name, msg.text)
+    });
+
+    const form = document.querySelector('form')
+    const name = document.querySelector('input[name="name"]')
+    const text = document.querySelector('input[name="text"]')
+    const ul = document.querySelector('ul')
+
+    form.addEventListener('submit', () => {
+      const [n, t] = [name.value, text.value]
+
+      ws.emit('sendChat', {  // sends to all clients
+        name: n,
+        text: t
+      });
+
+      displayChat(n, t);  // so not to rely on waiting on the server (think if this were a video game)
+
+      text.value = ''
+
+      event.preventDefault();  // prevents default form action so the page does not refresh
+    })
+
+    function displayChat (name, text) {
+      const li = generateLI(name, text)
+
+      ul.appendChild(li)
+    }
+
+    function generateLI (name, text) {
+      const li = document.createElement('li')
+      const textNode = document.createTextNode(`${name}: ${text}`)
+
+      li.appendChild(textNode)
+      return li
+    }
+}());
---
+++ server.js
@@ -0,0 +1,36 @@
+"use strict";
+
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);  // think this is wrapped around the Experss server because of 2 different protocols being used
+const ws = require('socket.io')(server);  // passing the server into socket.io ---> ws for 'websockets'
+
+const PORT = process.env.PORT || 3000;
+
+app.set('view engine', 'jade');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+// app.get('/', (req, res) => {
+//   res.send('hello');
+// });
+
+server.listen(PORT, () => {
+  console.log(`Server listening on PORT: ${PORT}`);
+});
+
+ws.on('connection', (socket => {  // callback fired every time a connection is made
+  console.log('*******  back-end connection *******', socket);
+
+  socket.on('sendChat', (msg) => {
+    console.log(msg);  // console log in server
+    // ws.emit('receieveChat', msg);  // emit to everyone else when a message is heard
+    socket.broadcast.emit('receiveChat', msg)  // this let's the msg appear on the sender's screen before being sent via server to others
+  });
+
+
+}));
